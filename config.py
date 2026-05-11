af03914eea2c73fe9305be87e19b1431637a7dc8
Vaishali <vv1705260@gmail.com>
2026-05-11 17:25:54 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+# =========================
+# CONFIGURATION FILE
+# =========================
+
+FILE_CATEGORIES = {
+    "Images": [".jpg", ".jpeg", ".png", ".gif", ".webp"],
+    "Documents": [".pdf", ".docx", ".txt", ".pptx", ".xlsx"],
+    "Videos": [".mp4", ".mkv", ".mov", ".avi"],
+    "Music": [".mp3", ".wav"],
+    "Programs": [".py", ".java", ".cpp", ".html", ".css", ".js"],
+    "Archives": [".zip", ".rar"],
+    "Applications": [".exe", ".msi"]
+}
+
+LOG_FOLDER = "logs"
+LOG_FILE = "logs/organizer.log"
+
+REPORT_FILE = "file_report.txt"
