16b23bb835a7940a77bd2410d18206120b102997
Vaishali <vv1705260@gmail.com>
2026-05-11 17:27:03 +0530

    Add show_banner function to display a banner

---
+++ banner.py
@@ -0,0 +1,7 @@
+def show_banner():
+    print("=" * 60)
+    print("        ADVANCED FILE ORGANIZER SYSTEM")
+    print("=" * 60)
+    print("   Organize Your Files Automatically")
+    print("   Developed Using Python")
+    print("=" * 60)
